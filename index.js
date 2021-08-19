8a98e52366768664cacdfc8cdf61ba11c6fa0e1d
Derek Araujo <derekaraujo19@gmail.com>
2021-08-19 18:46:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+
+function writeCards(names, event) {
+  let tyCards = [];
+  for (let i = 0; i < names.length; i++) {
+    tyCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return tyCards;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num--);
+  }
+}
